00f1c3b5a5f73ddd9809b600371b159278573b8a
Daniel Gilkarov <dgilkarov33@gmail.com>
2023-08-14 16:13:50 +0300

    Changed small issue with msb var in run_experiments.py

--- run_experiments.py
+++ run_experiments.py
@@ -2,13 +2,12 @@ from feature_extraction import create_grads_dataset, create_losses_dataset
 from classification import generate_supervised_results, generate_unsupervised_results
 
 def run_experiments(zoos, msb=False, malware_name="malware_12584bytes", n_jobs=-1):
-    
     for zoo in zoos:
-        #create_grads_dataset(zoo, malware_name=malware_name, msb=msb, n_jobs=n_jobs)
+        create_grads_dataset(zoo, malware_name=malware_name, msb=msb, n_jobs=n_jobs)
         create_losses_dataset(zoo, malware_name=malware_name, msb=msb)
 
-    generate_supervised_results(zoos=zoos, msb=True, n_jobs=n_jobs)
-    generate_unsupervised_results(zoos=zoos, msb=True)
+    generate_supervised_results(zoos=zoos, msb=msb, n_jobs=n_jobs)
+    generate_unsupervised_results(zoos=zoos, msb=msb)
 
 
 """You need to choose which zoos to run experiments on:
